38d1e55bc8cff91715605192299c0a73e50bc29a
Bjorn Harvold <bjorn@harvold.com>
2025-07-17 12:47:52 +0700

    fix: :arrow_up: upgraded starlight to 0.35 that fixes an i18n recursion bug

--- src/content/config.ts
+++ src/content/config.ts
@@ -1,6 +1,12 @@
-import { defineCollection } from 'astro:content';
-import { docsSchema } from '@astrojs/starlight/schema';
+import { defineCollection } from "astro:content";
+import { docsLoader } from '@astrojs/starlight/loaders';
+import { docsSchema } from "@astrojs/starlight/schema";
 
 export const collections = {
-	docs: defineCollection({ schema: docsSchema() }),
+  docs: defineCollection({
+    loader: docsLoader({
+     // Strip the `.md`/`.mdx` file extension, but don’t lowercase
+     generateId: ({ entry }) => entry.split('.').slice(0, -1).join('.')
+   }),
+    schema: docsSchema() }),
 };
